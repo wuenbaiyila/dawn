f60bcec8c1cc39f4dc9060920690baf3b90cb80c
lruiy <1472484877@qq.com>
2018-01-29 08:50:23 +0800

    一组 角色 服务js

--- dawn-web-manager/src/main/webapp/js/service/brandService.js
+++ dawn-web-manager/src/main/webapp/js/service/brandService.js
@@ -3,28 +3,28 @@ app.service('brandService',function($http){
 
 //抽取查询方法
     this.findAll=function(){
-        return $http.get('../brand/findAll.do');//业务逻辑层 service
+        return $http.get('../sysrole/findSysRoleAll.do');//业务逻辑层 service
     }
 
     //抽取删除的方法
-    this.delete=function(ids){
-        return $http.get('../brand/delete.do?ids='+ids);
+    this.delete=function(delIds){
+        return $http.get('../sysrole/deleteSysRole.do?delIds='+delIds);
     }
     //抽取添加的方法
     this.add=function(entity){
-        return $http.post('../brand/add.do',entity)
+        return $http.post('../sysrole/addSysRole.do',entity)
     }
     //抽取修改的方法
     this.update=function(entity){
-        return $http.post('../brand/update.do',entity)
+        return $http.post('../sysrole/updateSysRole.do',entity)
     }
     //抽取根据id查询
     this.findOne=function(id){
-        return $http.get('../brand/findOne.do?id='+id);
+        return $http.get('../sysrole/findOne.do?id='+id);
     }
     //抽取条件查询分页方法
     this.search=function(page,rows,searchEntity){
-        return $http.post('../brand/search.do?page='+page+"&rows="+rows,searchEntity);
+        return $http.post('../sysrole/findSysRoleAll.do?page='+page+"&rows="+rows,searchEntity);
     }
     //抽取分页方法
     this.findPage=function(page,rows){
